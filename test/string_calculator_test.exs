3c4afc60005e34cd75a5e29798c7f01d5dc2593f
Thanh Huynh <huynhbathanh@gmail.com>
2021-07-11 11:35:00 +0700

    Clean up test content

--- test/string_calculator_test.exs
+++ test/string_calculator_test.exs
@@ -7,7 +7,7 @@ defmodule StringCalculatorTest do
     assert StringCalculator.calc(" ") == 0
   end
 
-  test "return zero given nil value" do
+  test "returns zero given nil value" do
     assert StringCalculator.calc(nil) == 0
   end
 
@@ -15,29 +15,29 @@ defmodule StringCalculatorTest do
     assert StringCalculator.calc("1") == 1
   end
 
-  test "return 2 given 1,1" do
+  test "returns 2 given 1,1" do
     assert StringCalculator.calc("1,1") == 2
   end
 
-  test "raises Exception with Negative value" do
-    assert_raise ArgumentError, fn ->
-      assert StringCalculator.calc("1,-1")
-    end
+  test "returns 5 given 1,1,3" do
+    assert StringCalculator.calc("1,1,3") == 5
   end
 
-  test "ignores larger than 1k value" do
-    assert StringCalculator.calc("1001,2") == 2
-  end
-
-  test "ignoers multi delimiter" do
-    assert StringCalculator.calc("1***2***3") == 6
-  end
-
-  test "return 2 given string with breakline 1\n1" do
+  test "returns 2 given string with breakline 1\n1" do
     assert StringCalculator.calc("1\n1,1") == 3
   end
 
-  test "return 5 given 1,1,3" do
-    assert StringCalculator.calc("1,1,3") == 5
+  test "ignores multi delimiter" do
+    assert StringCalculator.calc("1***2***3") == 6
+  end
+
+  test "ignores big value given number larger than 1000" do
+    assert StringCalculator.calc("1001,2") == 2
+  end
+
+  test "raises ArgumentError given negative value" do
+    assert_raise ArgumentError, fn ->
+      assert StringCalculator.calc("1,-1")
+    end
   end
 end
