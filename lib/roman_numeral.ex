584b166518f8f063edffdaf45d3a234d0dc9edf2
Thanh Huynh <huynhbathanh@gmail.com>
2021-07-24 10:59:32 +0700

    Implement Roman Numeral

--- lib/roman_numeral.ex
+++ lib/roman_numeral.ex
@@ -1,13 +1,36 @@
 defmodule RomanNumeral do
+
+  @roman_mapping %{
+    1 => "I",
+    4 => "IV",
+    5 => "V",
+    10 => "X",
+    40 => "XL",
+    50 => "L",
+    90 => "XC",
+    100 => "C",
+    400 => "CD",
+    500 => "D",
+    900 => "CM",
+    1000 => "M"
+  }
+
+  # @numeral_list Map.keys(@roman_mapping)|> Enum.sort(&(&1 > &2))
+  @numeral_list Map.keys(@roman_mapping)
+
   def convert(number) do
-    cond do
-      number == 1 -> "I"
-      number == 2 -> "II"
-      number == 3 -> "III"
-      number == 5 -> "V"
-      number == 10 -> "X"
-      number == 20 -> "XX"
-      true -> "hi"
+    to_roman(@numeral_list, number, "")
+  end
+
+  defp to_roman(_mapping, 0, result) do
+    result
+  end
+
+  defp to_roman([max | rest] = numeral_list, number, result) do
+    if number >= max do
+      to_roman(numeral_list, number - max, result <> @roman_mapping[max])
+    else
+      to_roman(rest, number, result)
     end
   end
 end
